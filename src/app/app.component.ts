e7ad16833aa1e9d40bccf93cf673190b2e3208c7
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-01-27 21:12:14 +0100

    13-Peticones Htpp en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DoctorAppFrontendV1';
+  usuarios: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5038/api/Usuario').subscribe({
+
+      next: response => this.usuarios = response,
+      error: error => console.log(error),
+      complete: () => console.log('la solicitud esta completa')
+    })
+  }
 }
