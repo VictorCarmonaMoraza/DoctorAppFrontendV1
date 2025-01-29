388b1cf8e20f46d44e14cb8a4335e42275e855ee
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-01-29 19:38:55 +0100

    43-Mostrar usuario conectado y cerrar sesion

--- src/app/compartido/layout/layout.component.ts
+++ src/app/compartido/layout/layout.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CompartidoService } from '../services/compartido.service';
 
 @Component({
   selector: 'app-layout',
   standalone: false,
-  
+
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
 
+export class LayoutComponent implements OnInit {
+
+  username: string = '';
+
+  constructor(
+    private router: Router,
+    private compartidoService: CompartidoService
+  ) {
+  }
+
+  ngOnInit(): void {
+    const usuarioToken = this.compartidoService.obtenerSesion();
+    // if (usuarioToken != null) {
+    //   this.username = usuarioToken.username;
+    // }
+    this.ValidarUsuarioEnLocalStorage(usuarioToken);
+  }
+
+  ValidarUsuarioEnLocalStorage(usuario: any) {
+    if (usuario != null) {
+      this.username = usuario.username;
+    }
+  }
+
+  cerrarSesion(){
+    //Eliminamos usuario sesion
+    this.compartidoService.eliminarSesion();
+    //Redireccionamos al login
+    this.router.navigate(['login']);
+  }
 }
